ebf041d31d7b791a97aa6892eeb4de8ed44876f7
Joshua May <github@hotdog.horse>
2025-10-17 19:10:39 +0200

    horse 0.2.0

--- Formula/horse.rb
+++ Formula/horse.rb
@@ -8,7 +8,8 @@ class Horse < Formula
     root_url "https://github.com/notjosh/horse/releases/download/v0.2.0"
     sha256 cellar: :any_skip_relocation, arm64_sequoia: "0556f86ebbe559220bfa7b67b77aef448fbbb286ea063d7f4d7d75be19b48451"
     sha256 cellar: :any_skip_relocation, arm64_sonoma: "37a21265a09b04039ec8a01aa74874d3f5d423426e6d81369da05ce3ab25c21b"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe: "3640f6473edd44958dd8f389d5638fe1e13bd9050a2eba608ca3386ca3921c89"  end
+    sha256 cellar: :any_skip_relocation, arm64_tahoe: "3640f6473edd44958dd8f389d5638fe1e13bd9050a2eba608ca3386ca3921c89"
+  end
 
   depends_on "rust" => :build
 
