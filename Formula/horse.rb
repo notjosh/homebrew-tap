acb3f396b164fe83e7812fa01f053c87d7fb895e
Joshua May <github@hotdog.horse>
2025-10-17 19:24:38 +0200

    horse 0.3.0

--- Formula/horse.rb
+++ Formula/horse.rb
@@ -1,14 +1,14 @@
 class Horse < Formula
   desc "Display an animated ASCII art carousel of horsesbr"
   homepage "https://github.com/notjosh/horse"
-  url "https://github.com/notjosh/horse/archive/refs/tags/v0.2.1.tar.gz"
-  sha256 "dc5a9575053d6839d7c30d9895c580ef094c43850714687c3489290054bf3890"
+  url "https://github.com/notjosh/horse/archive/refs/tags/v0.3.0.tar.gz"
+  sha256 "9567dfcba7bb6fd1b95271be95a754391e71ff1a0f97c5248db2d28b144b28c9"
 
   bottle do
-    root_url "https://github.com/notjosh/horse/releases/download/v0.2.1"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "ecc34eae4c783bbfa5d10f22655b3a735f5f75d24f70333b49687510b6ae0f53"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma: "9c686cffdb76758f3c8a12883bd8c9371d69aac104958587acd3a3e1b9865bb2"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe: "21b47eead0dca9c053ffd0a0c37a1f2da813e709ebda00ce7b744608a5def408"
+    root_url "https://github.com/notjosh/horse/releases/download/v0.3.0"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "ca5323e6dc3e43df59c5ae94d8552e5e1cf7205099c170d2a46e1432050a472f"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma: "f4c83f7e5ebfe1a8a271dd6146647910eeb5b05f3b6e02c97be26e9ad8ec0ba0"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe: "c16b70bbdfdf58c51fd43c28fe83e1eb92192daf81ae3e1d5dd6a6270f8d638c"
   end
 
   depends_on "rust" => :build
